43ad66bf49217fd173937dcc7266d53a9fbf4b4d
Gareth Dawson <email@garethdawson.xyz>
2024-11-28 19:47:48 +0000

    51

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">change</p>
+      <p className="read-the-docs">more changes</p>
     </>
   );
 }
